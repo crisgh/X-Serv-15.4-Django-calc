85a0a80fa2e42c3d2ce65aafc464a6a4d7dd986f
Cristina Gallego <cristina@ubuntu>
2016-04-01 20:47:42 +0200

    urls completo

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^(\d+)(\+)(\d+)$', 'calc.views.sumar',),
+    url(r'^(\d+)(\-)(\d+)$', 'calc.views.restar',),
+    url(r'^(\d+)(\*)(\d+)$', 'calc.views.multiplicar',),
+    url(r'^(\d+)(\/)(\d+)$', 'calc.views.division',),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^.*$', 'calc.views.404',),
 )
